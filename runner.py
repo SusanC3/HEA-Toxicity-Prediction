8590dbfe7da2d8f05625811542779b21cb37f7f7
Susan Clay <susanclay333@gmail.com>
2023-02-13 17:35:54 -0800

    attempting kfold validation

--- runner.py
+++ runner.py
@@ -6,32 +6,40 @@ import pandas as pd
 import seaborn as sns
 from tqdm.notebook import tqdm
 import matplotlib.pyplot as plt
+import random
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, TensorDataset,random_split,SubsetRandomSampler, ConcatDataset
+from torch.nn import functional as F
+import torchvision
+from torchvision import datasets,transforms
+import torchvision.transforms as transforms
 
 from sklearn.preprocessing import MinMaxScaler    
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 from sklearn.metrics import mean_squared_error, r2_score
 
 import pdb
+
+
 
 #tell pytorch to use GPU if available
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda:0" if use_cuda else "cpu")
 torch.backends.cudnn.benchmark = True
 
-params = {'batch_size': 16,
+params = {'batch_size': 32,
             'shuffle': True, #shuffle order of data each train
             'num_workers': 6}
 max_epochs = 150
-LEARNING_RATE = 0.001
+LEARNING_RATE = 0.002
 NUM_FEATURES = 801
 
-training_data = IOWrapper.Dataset() #__init__ not called for some reason
-training_generator = DataLoader(training_data, **params)
+dataset = IOWrapper.Dataset() #__init__ not called for some reason
+#training_generator = DataLoader(training_data, **params)
 
 #i'll worry about validation later
 
@@ -43,90 +51,78 @@ print(model)
 criterion = nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
-loss_stats = [0 for i in range(max_epochs)]
+history = {'train_loss': [], 'test_loss': [], 'train_acc': [], 'test_acc': []}
 
-print("Begin training")
-model.train() #we're in training mode
-for epoch in range(max_epochs):
-    print("on epoch", epoch, "out of", max_epochs)
-    for input, output in training_generator:
-        #transfer to GPU
-        local_input, local_output = input.to(device), output.to(device)
+
+#kfold stuff
+k = 10
+splits = KFold(n_splits=k, shuffle=True, random_state=42)
+foldperf={}
+
+def train_epoch(model, device, dataloader, loss_fn, optimizer):
+    train_loss, train_correct = 0.0, 0
+    model.train()
+    
+    for input, output in dataloader:
+        input, output = input.to(device), output.to(device)
         optimizer.zero_grad()
 
-     #   pdb.set_trace()
-
-        y_train_pred = model(local_input)
-        train_loss = criterion(y_train_pred, local_output.unsqueeze(1))
-        train_loss.backward()
+        pred_result = model(input)
+        loss = loss_fn(pred_result, output)
+        loss.backward()
         optimizer.step()
+        train_loss += loss.item() * input.size(0)
+        scores, predictions = torch.max(pred_result.data, 1)
+        train_correct += (predictions == output).sum().item()
 
-        
-
-
-
-
-
+    return train_loss, train_correct
 
 
+def valid_epoch(model, device, dataloader, loss_fn):
+    valid_loss, val_correct = 0.0, 0
+    model.eval()
+    for input, output in dataloader:
+        input, output = input.to(device), output.to(device)
+        pred_result = model(input)
+        loss = loss_fn(pred_result, output)
+        valid_loss += loss.item() * input.size(0)
+        scores, predictions = torch.max(pred_result.data, 1)
+        val_correct +=(predictions == output).sum().item()
+
+    return valid_loss, val_correct
 
 
+print("Begin training")
 
 
+for fold, (train_idx, val_idx) in enumerate(splits.split(np.arange(5070))):
+
+    print('Fold {}'.format(fold + 1))
+
+    train_sampler = SubsetRandomSampler(train_idx)
+    test_sampler = SubsetRandomSampler(val_idx)
+    train_loader = DataLoader(dataset, batch_size=params['batch_size'], sampler=train_sampler)
+    test_loader = DataLoader(dataset, batch_size=params['batch_size'], sampler=test_sampler)
+
+    for epoch in range(max_epochs):
+        train_loss, train_correct = train_epoch(model, device, train_loader, criterion, optimizer)
+        test_loss, test_correct = valid_epoch(model, device, test_loader, criterion)
+
+        train_loss /= len(train_loader.sampler)
+        train_acc = train_correct / len(train_loader.sampler) * 100
+        test_loss /= len(test_loader.sampler)
+        test_acc = train_correct / len(test_loader.sampler) * 100
+
+        print("Epoch:{}/{} AVG Training Loss:{:.3f} AVG Test Loss:{:.3f} AVG Training Acc {:.2f} % AVG Test Acc {:.2f} %".format(epoch + 1,
+                                                                                                             max_epochs,
+                                                                                                             train_loss,
+                                                                                                             test_loss,
+                                                                                                             train_acc,
+                                                                                                             test_acc))
+
+        history['train_loss'].append(train_loss)
+        history['test_loss'].append(test_loss)
+        history['train_acc'].append(train_acc)
+        history['test_acc'].append(test_acc)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# from tensorflow.keras.models import Sequential
-# from tensorflow.keras.layers import Dense
-# from scikeras.wrappers import KerasRegressor
-# from sklearn.model_selection import cross_val_score
-# from sklearn.model_selection import KFold
-# from sklearn.preprocessing import StandardScaler
-# from sklearn.pipeline import Pipeline
-
-# import input_output
-
-# X, Y = input_output.get_input_output() #TODO get from feature selection
-
-# print("defining model")
-# def get_model():
-#     model = Sequential()
-#     model.add(Dense(5070, input_shape=(5070,), kernel_initializer='normal', activation='relu'))
-#     model.add(Dense(1, kernel_initializer='normal'))
-#     model.compile(loss='mean_squared_error', optimizer='adam')
-#     return model
-
-
-# print("standardizing data")
-# estimators = []
-# estimators.append(('standardize', StandardScaler()))
-# estimators.append(('mlp', KerasRegressor(model=get_model, epochs=50, batch_size = 128, verbose=1)))
-# pipeline = Pipeline(estimators)
-# print("Starting training & assessment")
-# #use kfold for now, in the futre want to use whole data with just this test data as test
-# kfold = KFold(n_splits=10)
-# print("After kfold thing")
-# results = cross_val_score(pipeline, X, Y, cv=kfold, scoring='neg_mean_squared_error')
-# print('Results:', results.mean(), "(", results.std(), ") MSE")
-# #TODO WILL NEED TO RE-SCALE OUTPUT FOR ACTUALLY CORRECT GUESSES
