d1d81614cb01f7e78a810e7800622c5179151e09
Susan Clay <susanclay333@gmail.com>
2023-04-20 13:54:37 -0700

    update naming

--- linear_model.py
+++ linear_model.py
@@ -6,21 +6,21 @@ import pdb
 dataset = Data.Dataset()
 
 #will fit to first 90% of data, score on last 10%
-X_fit = dataset.X[:int(0.9*len(dataset.X))]
-y_fit = dataset.y[:int(0.9*len(dataset.y))]
+X_train = dataset.X[:int(0.9*len(dataset.X))]
+y_train = dataset.y[:int(0.9*len(dataset.y))]
 
-X_score = dataset.X[int(0.9*len(dataset.X)):]
-y_score = dataset.y[int(0.9*len(dataset.y)):]
+X_test = dataset.X[int(0.9*len(dataset.X)):]
+y_test = dataset.y[int(0.9*len(dataset.y)):]
 
 model = Ridge()
-model.fit(X_fit, y_fit)
+model.fit(X_test, y_test)
 
-preds_fit = model.predict(X_fit)
-tot_squared_error = np.sum((preds_fit - y_fit)**2)
+preds_fit = model.predict(X_train)
+tot_squared_error = np.sum((preds_fit - y_train)**2)
 denominator = preds_fit.shape[0]*preds_fit.shape[1]
-print("MSE fit:", tot_squared_error/denominator)
+print("MSE train:", tot_squared_error/denominator)
 
-preds_score = model.predict(X_score)
-tot_squared_error = np.sum((preds_score - y_score)**2)
+preds_score = model.predict(X_test)
+tot_squared_error = np.sum((preds_score - y_test)**2)
 denominator = preds_score.shape[0]*preds_score.shape[1]
-print("MSE score:", tot_squared_error/denominator)
+print("MSE test:", tot_squared_error/denominator)
