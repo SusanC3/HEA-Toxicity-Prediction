7e5b14d41d0649def1d8973874da1384d07b5603
Susan Clay <susanclay333@gmail.com>
2023-05-17 15:08:27 -0700

    get rid of some old files

--- linear_model.py
+++ linear_model.py
@@ -15,7 +15,7 @@ mse_tests = []
 mse_trains = []
 
 def MSE_from_alpha(a):
-    for fold, (train_idx, val_idx) in enumerate(splits.split(np.arange(5070))):
+    for fold, (train_idx, val_idx) in enumerate(splits.split(np.arange(1724))):
         X_train, y_train = [], []
         for id in train_idx:
             X_train.append(dataset.__getitem__(id)[0])
@@ -32,15 +32,17 @@ def MSE_from_alpha(a):
         preds_fit = model.predict(X_train)
         tot_squared_error = np.sum((preds_fit - y_train)**2)
         denominator = preds_fit.shape[0]#*preds_fit.shape[1]
-        #print("MSE train:", tot_squared_error/denominator)
+        print("MSE train:", tot_squared_error/denominator)
         mse_trains.append(tot_squared_error/denominator)
 
         preds_score = model.predict(X_test)
         tot_squared_error = np.sum((preds_score - y_test)**2)
         denominator = preds_score.shape[0]#*preds_score.shape[1]
-        #print("MSE test:", tot_squared_error/denominator)
+        print("MSE test:", tot_squared_error/denominator)
         if (tot_squared_error/denominator < 1):
           mse_tests.append(tot_squared_error/denominator)
+
+        print()
       
 
 print("Starting fit")
--- temp.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import pdb
-
-df = pd.read_csv("assays/AID_1671196_p450-2d6-p1.csv", skiprows=[i for i in range(50, 9667)])
-df = df.drop([df.columns[i] for i in range(100, 2049)], axis=1)
-
-#pdb.set_trace()
-
-df.to_csv("shortened_p450.csv")
